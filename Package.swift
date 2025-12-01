3030578b16b8cc33a1fddcdbb1587de480bdf7b6
Abhishek Warokar <abhishek11@live.in>
2025-12-01 13:14:49 +0530

    Update tools version

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.10
 import PackageDescription
 
 let package = Package(
     name: "FinvuSDK",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v16)
     ],
     products: [
         .library(
